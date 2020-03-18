5925e2d8fb0ca748778f762a46d75dd67adeba2a
emanuele.galeotti <emanuele.galeotti@quix.it>
2020-03-18 18:41:14 +0100

    Insert action label

--- projects/quix-core-components/src/lib/data-table/action-data-table.model.ts
+++ projects/quix-core-components/src/lib/data-table/action-data-table.model.ts
@@ -1,14 +1,16 @@
 export class ActionDataTableModel {
+  public actionLabel: string;
   public actionName: string;
   public icon: Array<string>;
   public condition?: (element: any) => {};
 
-  constructor(actionName: string, icon: Array<string>, condition?: (element: any) => {}) {
+  constructor(actionLabel: string, actionName: string, icon: Array<string>, condition?: (element: any) => {}) {
+    this.actionLabel = actionLabel;
     this.actionName = actionName;
     this.icon = icon;
     if (!condition) {
       this.condition = (element: any) => {
-        return false;
+        return true;
       };
     } else {
       this.condition = condition;
