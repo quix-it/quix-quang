95378a7334148e24a73e8fa95968d596f9132dbc
manuel.pirrone <manuel.pirrone@quix.it>
2022-04-05 17:03:19 +0200

    Fix(input-time-component): fix input-time component
    Fix input-time style, fix validation

--- projects/quang-date/src/lib/input-time/input-time.component.spec.ts
+++ projects/quang-date/src/lib/input-time/input-time.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { InputTimeComponent } from './input-time.component'
 
@@ -6,12 +6,12 @@ describe('InputTimeComponent', () => {
   let component: InputTimeComponent
   let fixture: ComponentFixture<InputTimeComponent>
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [InputTimeComponent]
     })
       .compileComponents()
-  }))
+  })
 
   beforeEach(() => {
     fixture = TestBed.createComponent(InputTimeComponent)
