3811e2b8f76e550b94387dd6a728b98e33d04d2d
manuel.pirrone <manuel.pirrone@quix.it>
2022-09-16 15:07:24 +0200

    feat(dv): add defaultValue to checkbox, datalist

--- projects/quang-core/src/lib/input-checkbox/input-checkbox.component.ts
+++ projects/quang-core/src/lib/input-checkbox/input-checkbox.component.ts
@@ -9,137 +9,146 @@ import {
   Renderer2,
   Self,
   SimpleChanges,
-  ViewChild
-} from '@angular/core'
-import { ControlValueAccessor, NgControl } from '@angular/forms'
-import { delay, filter } from 'rxjs/operators'
+  ViewChild,
+} from "@angular/core";
+import { ControlValueAccessor, NgControl } from "@angular/forms";
+import { delay, filter } from "rxjs/operators";
 
 /**
  * input checkbox component decorator
  */
 @Component({
-  selector: 'quang-input-checkbox',
-  templateUrl: './input-checkbox.component.html',
-  styleUrls: ['./input-checkbox.component.scss']
+  selector: "quang-input-checkbox",
+  templateUrl: "./input-checkbox.component.html",
+  styleUrls: ["./input-checkbox.component.scss"],
 })
 /**
  * input checkbox component
  */
-export class InputCheckboxComponent implements OnInit, ControlValueAccessor, AfterViewInit, OnChanges {
+export class InputCheckboxComponent
+  implements OnInit, ControlValueAccessor, AfterViewInit, OnChanges
+{
   /**
    * Html id of input
    */
-  @Input() id: string = ''
+  @Input() id: string = "";
   /**
    * The label to display on the input field
    */
-  @Input() label: string = ''
+  @Input() label: string = "";
   /**
    * Defines whether the checkbox should align with the other checkboxes next to it
    */
-  @Input() inline: boolean = false
+  @Input() inline: boolean = false;
   /**
    * Determine the arialabel tag for accessibility,
    * If not specified, it takes 'input' concatenated to the label by default
    */
-  @Input() ariaLabel: string = `Input ${this.label}`
+  @Input() ariaLabel: string = `Input ${this.label}`;
   /**
    * Indicates whether, when the page is opened,
    * this input field should be displayed in a focused state or not
    */
-  @Input() autofocus: boolean = false
+  @Input() autofocus: boolean = false;
   /**
    * Indicate the position in the page navigation flow with the tab key
    */
-  @Input() tabIndex: number = 0
+  @Input() tabIndex: number = 0;
   /**
    * Defines if you want to display the success message for the user
    */
-  @Input() successMessage: boolean = false
+  @Input() successMessage: boolean = false;
   /**
    * Defines if you want to display the error message for the user
    */
-  @Input() errorMessage: boolean = false
+  @Input() errorMessage: boolean = false;
   /**
    * Defines if you want to display the help message for the user
    */
-  @Input() helpMessage: boolean = false
+  @Input() helpMessage: boolean = false;
   /**
    * The name of the form, this input is used to create keys for error, validation or help messages.
    * It will be the first key element generated
    */
-  @Input() formName: string = ''
+  @Input() formName: string = "";
   /**
    * Array of additional classes to the input field
    */
-  @Input() customClass: string[] = []
+  @Input() customClass: string[] = [];
   /**
    * Defines the autocomplete tag to indicate to the browser what type of field it is
    * and how to help the user fill it in
    */
-  @Input() autocomplete: string = 'off'
+  @Input() autocomplete: string = "off";
+  /**
+   * set default start value in select option
+   */
+  @Input() defaultValue: any;
 
   /**
    * The value of the input
    */
-  _value: boolean = false
+  _value: boolean = false;
   /**
    * the status of the success message
    */
-  _successMessage: string = ''
+  _successMessage: string = "";
   /**
    * the status of the error message
    */
-  _errorMessage: string = ''
+  _errorMessage: string = "";
   /**
    * the status of the help message
    */
-  _helpMessage: string = ''
+  _helpMessage: string = "";
   /**
    * Contains the value required by a validation when it fails
    */
-  _requiredValue: any = ''
+  _requiredValue: any = "";
   /**
    * Define disabled state
    */
-  _disabled: boolean = false
+  _disabled: boolean = false;
   /**
    * The html input element
    */
-  @ViewChild('input', { static: true }) input: ElementRef<HTMLInputElement> | null = null
+  @ViewChild("input", { static: true })
+  input: ElementRef<HTMLInputElement> | null = null;
   /**
    * Standard definition to create a control value accessor
    */
-  onTouched: any = () => {
-  }
+  onTouched: any = () => {};
 
   /**
    * Standard definition to create a control value accessor
    */
-  onChanged: any = () => {
-  }
+  onChanged: any = () => {};
 
   /**
    * constructor
    * @param renderer html access
    * @param control cva access
    */
-  constructor (
+  constructor(
     private readonly renderer: Renderer2,
     @Self() @Optional() public control: NgControl
   ) {
-    this.control.valueAccessor = this
+    this.control.valueAccessor = this;
   }
 
   /**
    * Check if the help message is required and create the key
    */
-  ngOnInit (): void {
+  ngOnInit(): void {
     if (this.helpMessage) {
-      this._helpMessage = `${this.formName}.${this.control?.name}.help`
+      this._helpMessage = `${this.formName}.${this.control?.name}.help`;
     }
     if (this.successMessage) {
-      this._successMessage = `${this.formName}.${this.control?.name}.valid`
+      this._successMessage = `${this.formName}.${this.control?.name}.valid`;
+    }
+    if (this.defaultValue) {
+      this.writeValue(this.defaultValue);
+      this.onChanged(this._value);
     }
   }
 
@@ -147,37 +156,37 @@ export class InputCheckboxComponent implements OnInit, ControlValueAccessor, Aft
    * After rendering the component, it checks if the input field must have focus
    * and activates the monitoring of the validation of the entered values
    */
-  ngAfterViewInit (): void {
+  ngAfterViewInit(): void {
     setTimeout(() => {
       if (this.autofocus) {
-        this.input?.nativeElement.focus()
+        this.input?.nativeElement.focus();
       }
-    }, 0)
-    this.observeValidate()
+    }, 0);
+    this.observeValidate();
   }
 
   /**
    * Add focus to the input field if the need comes after component initialization
    * @param changes component changes
    */
-  ngOnChanges (changes: SimpleChanges): void {
+  ngOnChanges(changes: SimpleChanges): void {
     if (changes.autofocus?.currentValue && this.input) {
-      this.input.nativeElement.focus()
+      this.input.nativeElement.focus();
     }
   }
 
   /**
    * Standard definition to create a control value accessor
    */
-  registerOnTouched (fn: any): void {
-    this.onTouched = fn
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
   }
 
   /**
    * Standard definition to create a control value accessor
    */
-  registerOnChange (fn: any): void {
-    this.onChanged = fn
+  registerOnChange(fn: any): void {
+    this.onChanged = fn;
   }
 
   /**
@@ -185,28 +194,32 @@ export class InputCheckboxComponent implements OnInit, ControlValueAccessor, Aft
    * its value is retrieved from the html element and the status change is signaled to the form
    * @param e
    */
-  onChangedHandler (e: Event): void {
-    this._value = (e.target as HTMLInputElement).checked
-    this.onTouched()
-    this.onChanged(this._value)
+  onChangedHandler(e: Event): void {
+    this._value = (e.target as HTMLInputElement).checked;
+    this.onTouched();
+    this.onChanged(this._value);
   }
 
   /**
    * When the CVA is initialized as control it initializes the internal states
    * @param value
    */
-  writeValue (value: boolean): void {
-    this._value = value
-    this.renderer.setProperty(this.input?.nativeElement, 'checked', value)
+  writeValue(value: boolean): void {
+    this._value = value;
+    this.renderer.setProperty(this.input?.nativeElement, "checked", value);
   }
 
   /**
    * Standard definition to create a control value accessor
    * When the input field from the form is disabled, the html input tag is defined as disabled
    */
-  setDisabledState (isDisabled: boolean): void {
-    this._disabled = isDisabled
-    this.renderer.setProperty(this.input?.nativeElement, 'disabled', isDisabled)
+  setDisabledState(isDisabled: boolean): void {
+    this._disabled = isDisabled;
+    this.renderer.setProperty(
+      this.input?.nativeElement,
+      "disabled",
+      isDisabled
+    );
   }
 
   /**
@@ -215,17 +228,19 @@ export class InputCheckboxComponent implements OnInit, ControlValueAccessor, Aft
    * If there is an error with a specific required value it is passed to the translation pipe
    * to allow for the creation of custom messages
    */
-  observeValidate (): void {
-    this.control?.statusChanges?.pipe(
-      delay(0),
-      filter(() => !!this.control.dirty)
-    ).subscribe(() => {
-      if (this.control.invalid && this.errorMessage) {
-        for (const error in this.control.errors) {
-          this._requiredValue = this.control.errors[error].requiredValue
-          this._errorMessage = `${this.formName}.${this.control?.name}.${error}`
+  observeValidate(): void {
+    this.control?.statusChanges
+      ?.pipe(
+        delay(0),
+        filter(() => !!this.control.dirty)
+      )
+      .subscribe(() => {
+        if (this.control.invalid && this.errorMessage) {
+          for (const error in this.control.errors) {
+            this._requiredValue = this.control.errors[error].requiredValue;
+            this._errorMessage = `${this.formName}.${this.control?.name}.${error}`;
+          }
         }
-      }
-    })
+      });
   }
 }
