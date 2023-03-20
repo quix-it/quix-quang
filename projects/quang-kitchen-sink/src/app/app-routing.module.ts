5fd8e367dce7ba180820542e1502159342fba27f
manuel.pirrone <manuel.pirrone@quix.it>
2023-03-20 09:44:48 +0100

    fix: app.routing

--- projects/quang-kitchen-sink/src/app/app-routing.module.ts
+++ projects/quang-kitchen-sink/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'js',
-    loadChildren: async () =>
+    loadChildren: async (): Promise<any> =>
       await import('./training-js/training-js.module').then(
         (m) => m.TrainingJsModule
       )
