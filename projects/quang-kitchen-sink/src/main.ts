15be0643a8bb72760ab7c80e5bce3520a4a2497a
manuel.pirrone <manuel.pirrone@quix.it>
2023-03-21 15:56:41 +0100

    fix: codemirror

--- projects/quang-kitchen-sink/src/main.ts
+++ projects/quang-kitchen-sink/src/main.ts
@@ -4,10 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from './app/app.module'
 import { environment } from './environments/environment'
 
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/markdown/markdown'
+
 if (environment.production) {
   enableProdMode()
 }
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
-  .catch((err) => console.error(err))
+  .catch((err) => { console.error(err) })
