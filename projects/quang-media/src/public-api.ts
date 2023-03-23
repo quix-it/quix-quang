7a396a9d42b6c4b20633cc89bcde9e1424435e8b
manuel.pirrone <manuel.pirrone@quix.it>
2023-03-23 10:53:07 +0100

    fix: public-api

--- projects/quang-media/src/public-api.ts
+++ projects/quang-media/src/public-api.ts
@@ -8,5 +8,4 @@ export * from './lib/three-sixty-image/three-sixty-image.component'
 export * from './lib/video/video.component'
 export * from './lib/audio/audio.component'
 export * from './lib/code-reader/code-reader.component'
-
-export * from './lib/quang-media.module'
+export * from './lib/webcam/webcam.component'
