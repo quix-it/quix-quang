fdb28073e7cc4b17d291cc66afc3205d6cf9f967
manuel.pirrone <manuel.pirrone@quix.it>
2023-03-23 11:46:29 +0100

    fix: create new script to build quang

--- projects/quang-media/src/lib/quang-media.module.ts
+++ projects/quang-media/src/lib/quang-media.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core'
 import { PictureComponent } from './picture/picture.component'
 import { ThreeSixtyImageComponent } from './three-sixty-image/three-sixty-image.component'
 import { LazyLoadImageModule } from 'ng-lazyload-image'
-import { CommonModule } from '@angular/common'
 import { TranslocoModule } from '@ngneat/transloco'
 import { VideoComponent } from './video/video.component'
 import { AudioComponent } from './audio/audio.component'
 import { CodeReaderComponent } from './code-reader/code-reader.component'
 import { FormsModule } from '@angular/forms'
 import { WebcamComponent } from './webcam/webcam.component'
+import { CommonModule } from '@angular/common'
 
 @NgModule({
   declarations: [
