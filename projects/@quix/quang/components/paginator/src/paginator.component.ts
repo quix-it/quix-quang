8033b2d83ac1718c8fcccc257e39bf8fc9129079
Stefano Restuccia <stefano.restuccia@quix.it>
2023-10-13 14:05:17 +0200

    fix(paginator): paginator change page event

--- projects/@quix/quang/components/paginator/src/paginator.component.ts
+++ projects/@quix/quang/components/paginator/src/paginator.component.ts
@@ -120,6 +120,7 @@ export class QuangPaginatorComponent {
    */
   goToFirstPage(): void {
     this.page = 1
+    this.whenPageChange.emit(this.page)
     this.changeDetectorRef.detectChanges()
   }
 
