62f5a81c5f7f05b82817f8a26c592019ae90b95a
manuel.pirrone <manuel.pirrone@quix.it>
2023-11-29 16:39:38 +0100

    fix: autocomplete obj

--- projects/@quix/quang/components/autocomplete/object/src/autocomplete-obj.component.ts
+++ projects/@quix/quang/components/autocomplete/object/src/autocomplete-obj.component.ts
@@ -259,6 +259,8 @@ export class QuangAutocompleteObjectComponent implements ControlValueAccessor, O
     this._value = value
     if (this.dataList?.find((item) => item[this.returnValue] === value)) {
       this._searchValue = this.dataList?.find((item) => item[this.returnValue] === value)[this.searchBy]
+    } else {
+      this._searchValue = ''
     }
     this.renderer.setProperty(this.input?.nativeElement, 'value', this._searchValue)
   }
