3d0c7b1985778ca9c1efe10b55c49c2272d9a5c3
Stefano Restuccia <stefano.restuccia@quix.it>
2024-03-25 15:48:07 +0100

    feat(quang): added lib

---
+++ projects/quang/src/lib/quang.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'lib-quang',
+  standalone: true,
+  imports: [],
+  template: ` <p>quang works!</p> `,
+  styles: ``
+})
+export class QuangComponent {}
---
+++ projects/quang/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of quang
+ */
+
+export * from './lib/quang.service'
+export * from './lib/quang.component'
