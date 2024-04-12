f0c5a52feb5537691c87021086aef952296e6749
Stefano Restuccia <stefano.restuccia@quix.it>
2024-04-12 12:43:56 +0200

    feat(select): scaffold select component

--- projects/quang/components/select/select.component.ts
+++ projects/quang/components/select/select.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component, forwardRef } from '@angular/core'
+import { NG_VALUE_ACCESSOR } from '@angular/forms'
+
+import { QuangBaseComponent } from '../shared'
+
+export interface SelectOption {
+  label: string
+  value: string | number
+}
 
 @Component({
   selector: 'quang-select',
   standalone: true,
   imports: [],
   templateUrl: './select.component.html',
-  styleUrl: './select.component.scss'
+  styleUrl: './select.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true
+    }
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SelectComponent {}
+export class SelectComponent extends QuangBaseComponent<string | number | string[] | number[]> {
+  // input array di SelectionOption
+  // input mode "single", "multiple" con "single" come default
+}
