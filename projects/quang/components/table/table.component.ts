009d43d883f53b17fee36991c0370a69da4ac79d
Stefano Restuccia <stefano.restuccia@quix.it>
2024-04-23 17:47:28 +0200

    feat(table): base for table

--- projects/quang/components/table/table.component.ts
+++ projects/quang/components/table/table.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core'
+import { NgClass, NgIf } from '@angular/common'
+import { ChangeDetectionStrategy, Component, forwardRef, input } from '@angular/core'
+import { toObservable } from '@angular/core/rxjs-interop'
+import { NG_VALUE_ACCESSOR } from '@angular/forms'
+
+import { TranslocoPipe } from '@ngneat/transloco'
+
+import { QuangBaseComponent } from '@quix/quang/components/shared'
+
+export type InputType = 'text' | 'textarea' | 'password' | 'email' | 'number' | 'url' | 'search' | 'tel' | 'color'
 
 @Component({
-  selector: 'quang-table',
+  selector: 'quang-input',
   standalone: true,
-  imports: [],
-  templateUrl: './table.component.html',
-  styleUrl: './table.component.scss'
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => QuangInputComponent),
+      multi: true
+    }
+  ],
+  imports: [TranslocoPipe, NgIf, NgClass],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TableComponent {}
+export class QuangInputComponent extends QuangBaseComponent<string | number> {
+  componentType = input.required<InputType>()
+
+  constructor() {
+    super()
+    toObservable(this.componentType)
+      .pipe(this._takeUntilDestroyed())
+      .subscribe(() => {
+        this.setupFormControl()
+      })
+  }
+}
