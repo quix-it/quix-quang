3ab8b4cfcb9def290a2854f1073f65c1fd816017
Stefano Restuccia <stefano.restuccia@quix.it>
2024-04-30 17:07:49 +0200

    refactor(providers): providers functions import

--- projects/quang/translation/translation.service.ts
+++ projects/quang/translation/translation.service.ts
@@ -3,16 +3,16 @@ import { toSignal } from '@angular/core/rxjs-interop'
 
 import { HashMap, TranslocoService } from '@ngneat/transloco'
 
-import { AVAILABLE_LANGS, DEFAULT_LANG } from './translation.module'
+import { AVAILABLE_LANGS, DEFAULT_LANG } from './translation-providers'
 
 @Injectable()
 export class QuangTranslationService {
   activeLang = output<string | undefined>()
+  private readonly _translocoService: TranslocoService = inject(TranslocoService)
+  _langChanges = toSignal(this._translocoService.langChanges$)
   langChangesEffect = effect(() => {
     this.activeLang.emit(this._langChanges())
   })
-  private readonly _translocoService: TranslocoService = inject(TranslocoService)
-  _langChanges = toSignal(this._translocoService.langChanges$)
   private readonly _availableLangs = inject(AVAILABLE_LANGS)
   private readonly _defaultLang = inject(DEFAULT_LANG)
 
