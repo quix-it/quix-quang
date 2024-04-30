37d90186e4e7266a836e731421a102bc40521354
manuel.pirrone <manuel.pirrone@quix.it>
2024-04-30 11:51:19 +0200

    fix: popover style

--- projects/quang/overlay/popover/popover.component.ts
+++ projects/quang/overlay/popover/popover.component.ts
@@ -1,11 +1,11 @@
 import { ConnectionPositionPair } from '@angular/cdk/overlay'
-import { JsonPipe, NgComponentOutlet, NgTemplateOutlet } from '@angular/common'
+import { NgClass, NgComponentOutlet, NgTemplateOutlet } from '@angular/common'
 import { ChangeDetectionStrategy, Component, TemplateRef, input, signal } from '@angular/core'
 
 @Component({
   selector: 'quang-popover',
   standalone: true,
-  imports: [NgComponentOutlet, NgTemplateOutlet, JsonPipe],
+  imports: [NgComponentOutlet, NgTemplateOutlet, NgClass],
   templateUrl: './popover.component.html',
   styleUrl: './popover.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -13,4 +13,17 @@ import { ChangeDetectionStrategy, Component, TemplateRef, input, signal } from '
 export class QuangPopoverComponent {
   content = input<TemplateRef<any> | null>(null)
   positionPair = signal<ConnectionPositionPair | null>(null)
+
+  getPopoverPosition(): string {
+    const originX = this.positionPair()?.originX
+    const originY = this.positionPair()?.originY
+    if (originX && originY) {
+      if (originX === 'center') {
+        return originY
+      } else if (originY === 'center') {
+        return originX
+      }
+    }
+    return 'top'
+  }
 }
