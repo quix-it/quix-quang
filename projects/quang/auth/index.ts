7b7354e2df3e7f728b4e9877ba3a069f0eea570b
Stefano Restuccia <stefano.restuccia@quix.it>
2024-05-24 10:45:43 +0200

    fix(build): fixed package export

---
+++ projects/quang/auth/index.ts
@@ -0,0 +1,6 @@
+export * from './auth-providers'
+export * from './auth.service'
+export * from './directives/is-authenticated.directive'
+export * from './directives/has-at-least-one-role.directive'
+export * from './directives/has-every-role.directive'
+export * from './directives/is-not-authenticated.directive'
