a642b1a44deebb00047f3e102c6d41526f4f1bb7
Matteo Cassano <matteo.cassano@quix.it>
2024-07-18 15:01:11 +0200

    feat(translation): expose more methods from transloco, init activeLang with null

--- projects/quang/translation/translation.service.ts
+++ projects/quang/translation/translation.service.ts
@@ -1,35 +1,40 @@
-import { Injectable, effect, inject, output } from '@angular/core'
+import { Injectable, effect, inject, signal } from '@angular/core'
 import { toSignal } from '@angular/core/rxjs-interop'
 
-import { HashMap, TranslocoService } from '@jsverse/transloco'
+import { HashMap, SetTranslationOptions, Translation, TranslocoService } from '@jsverse/transloco'
 
 import { AVAILABLE_LANGS, DEFAULT_LANG } from './translation-providers'
 
 @Injectable()
 export class QuangTranslationService {
-  activeLang = output<string | undefined>()
+  activeLang = signal<string | null>(null)
 
   private readonly _translocoService: TranslocoService = inject(TranslocoService)
-  _langChanges = toSignal(this._translocoService.langChanges$)
+  _langChanges = toSignal(this._translocoService.langChanges$, { initialValue: null })
   langChangesEffect = effect(() => {
-    this.activeLang.emit(this._langChanges())
+    this.activeLang.set(this._langChanges())
   })
   private readonly _availableLangs = inject(AVAILABLE_LANGS)
   private readonly _defaultLang = inject(DEFAULT_LANG)
 
   setActiveLang(lang: string): void {
-    let targetLang = lang
-    if (!this._availableLangs.includes(lang)) {
-      targetLang = this._defaultLang
-    }
+    const targetLang = this._availableLangs.includes(lang) ? lang : this._defaultLang
     this._translocoService.setActiveLang(targetLang)
   }
 
-  getActiveLang(): string | undefined {
+  getActiveLang(): string | null {
     return this._langChanges()
   }
 
   translate(key: string, params?: HashMap): string {
     return this._translocoService.translate(key, params)
+  }
+
+  setTranslation(translation: Translation, lang?: string, options?: SetTranslationOptions) {
+    return this._translocoService.setTranslation(translation, lang, options)
+  }
+
+  setTranslationKey(key: string, value: string, options?: Omit<SetTranslationOptions, 'merge'>) {
+    return this._translocoService.setTranslationKey(key, value, options)
   }
 }
