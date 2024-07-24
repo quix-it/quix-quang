b8290b59b7f46cab06c7a74c4265ffa00069f9df
manuel.pirrone <manuel.pirrone@quix.it>
2024-07-24 15:39:48 +0200

    fix: popover if null

--- projects/quang/overlay/popover/popover.component.ts
+++ projects/quang/overlay/popover/popover.component.ts
@@ -1,11 +1,11 @@
 import { ConnectionPositionPair } from '@angular/cdk/overlay'
-import { NgClass, NgComponentOutlet, NgTemplateOutlet } from '@angular/common'
+import { NgClass, NgComponentOutlet, NgIf, NgTemplateOutlet } from '@angular/common'
 import { ChangeDetectionStrategy, Component, TemplateRef, input, signal } from '@angular/core'
 
 @Component({
   selector: 'quang-popover',
   standalone: true,
-  imports: [NgComponentOutlet, NgTemplateOutlet, NgClass],
+  imports: [NgComponentOutlet, NgTemplateOutlet, NgClass, NgIf],
   templateUrl: './popover.component.html',
   styleUrl: './popover.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -18,8 +18,6 @@ export class QuangPopoverComponent {
   getPopoverPosition(): string {
     const originX = this.positionPair()?.originX
     const originY = this.positionPair()?.originY
-    console.log('originX', originX)
-    console.log('originY', originY)
     if (originX && originY) {
       return `${originX}-${originY}`
     }
