aaf68e07f6233028fcd9e4826e5877827e9d1e6c
Matteo Cassano <matteo.cassano@quix.it>
2024-07-25 15:16:56 +0200

    feat: add mobile auth, edit scripts for beta publish

--- projects/quang/auth/auth-providers.ts
+++ projects/quang/auth/auth-providers.ts
@@ -1,14 +1,16 @@
-import { APP_INITIALIZER, EnvironmentProviders, makeEnvironmentProviders } from '@angular/core'
+import { APP_INITIALIZER, EnvironmentProviders, Provider, makeEnvironmentProviders } from '@angular/core'
 
 import { provideOAuthClient } from 'angular-oauth2-oidc'
 
 import { AUTH_CONFIG, QuangAuthConfig, QuangAuthService } from './auth.service'
 
+import { type MobileAuthFeature } from './mobile'
+
 function initializeAuthService(authService: QuangAuthService) {
   return () => authService.init()
 }
 
-export function provideAuth(authConfig?: QuangAuthConfig): EnvironmentProviders {
+export function provideAuth(authConfig?: QuangAuthConfig, ...features: QuangAuthFeatures[]): EnvironmentProviders {
   return makeEnvironmentProviders([
     {
       provide: AUTH_CONFIG,
@@ -25,6 +27,46 @@ export function provideAuth(authConfig?: QuangAuthConfig): EnvironmentProviders 
       useFactory: initializeAuthService,
       multi: true,
       deps: [QuangAuthService]
-    }
+    },
+    features.map((feature) => feature.ɵproviders)
   ])
+}
+
+/**
+ * Helper type to represent a QuangAuth feature.
+ *
+ * @publicApi
+ */
+export interface QuangAuthFeature<FeatureKind extends QuangAuthFeatureKind> {
+  ɵkind: FeatureKind
+  ɵproviders: Provider[]
+}
+
+/**
+ * Helper function to create an object that represents a Router feature.
+ */
+export function quangAuthFeature<FeatureKind extends QuangAuthFeatureKind>(
+  kind: FeatureKind,
+  providers: Provider[]
+): QuangAuthFeature<FeatureKind> {
+  return { ɵkind: kind, ɵproviders: providers }
+}
+
+/**
+ * A type alias that represents all QuangAuth features available for use with `provideAuth`.
+ * Features can be enabled by adding special functions to the `provideAuth` call.
+ * See documentation for each symbol to find corresponding function name. See also `provideAuth`
+ * documentation on how to use those functions.
+ *
+ * @see {@link provideAuth}
+ *
+ * @publicApi
+ */
+export type QuangAuthFeatures = MobileAuthFeature
+
+/**
+ * The list of features as an enum to uniquely type each feature.
+ */
+export const enum QuangAuthFeatureKind {
+  MobileAuthFeature
 }
---
+++ projects/quang/auth/mobile/mobile-auth-feature.ts
@@ -0,0 +1,56 @@
+import { APP_INITIALIZER, Injector, NgZone, Provider } from '@angular/core'
+
+import { App, URLOpenListenerEvent } from '@capacitor/app'
+import { Browser } from '@capacitor/browser'
+import { Capacitor } from '@capacitor/core'
+
+import { AUTH_CONFIG, QuangAuthService } from '../auth.service'
+
+import { QuangAuthFeature, QuangAuthFeatureKind, quangAuthFeature } from '../auth-providers'
+
+/**
+ * A type alias for providers returned by `withMobileAuth` for use with `provideAuth`.
+ *
+ * @see {@link withMobileAuth}
+ * @see {@link provideAuth}
+ *
+ * @publicApi
+ */
+export type MobileAuthFeature = QuangAuthFeature<QuangAuthFeatureKind.MobileAuthFeature>
+
+export function withMobileAuth(): MobileAuthFeature {
+  const providers: Provider[] = [
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      deps: [QuangAuthService, NgZone],
+      useFactory: (authService: QuangAuthService, ngZone: NgZone) => {
+        if (Capacitor.isNativePlatform()) {
+          App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+            ngZone.run(() => {
+              Browser.removeAllListeners()
+              Browser.close()
+              const [, queryParams] = event.url.split('?')
+              location.href = `/?${queryParams}`
+            })
+          })
+          App.addListener('resume', () => {
+            ngZone.run(() => {
+              authService.init()
+            })
+          })
+        }
+      }
+    },
+    {
+      provide: AUTH_CONFIG,
+      deps: [Injector],
+      useFactory: (injector: Injector) => {
+        const existingConfig = injector.get(AUTH_CONFIG)
+        if (!existingConfig) throw new Error('Missing QuangAuthConfig to extend')
+        return existingConfig
+      }
+    }
+  ]
+  return quangAuthFeature(QuangAuthFeatureKind.MobileAuthFeature, providers)
+}
