c61269b7c8b73ee2edbbb2ec528719499ce0a38c
Stefano Restuccia <stefano.restuccia@quix.it>
2024-11-08 17:59:24 +0100

    feat(autocomplete): removed null

--- projects/quang/components/autocomplete/autocomplete.component.ts
+++ projects/quang/components/autocomplete/autocomplete.component.ts
@@ -36,7 +36,7 @@ import { QuangBaseComponent, QuangOptionListComponent, SelectOption } from '@qui
  *
  * `searchTextDebounce` is by default set to 300ms.
  */
-export class QuangAutocompleteComponent extends QuangBaseComponent<string | number | null> {
+export class QuangAutocompleteComponent extends QuangBaseComponent<string | number> {
   syncFormWithText = input<boolean>(false)
 
   optionListMaxHeight = input<string>('200px')
@@ -52,7 +52,7 @@ export class QuangAutocompleteComponent extends QuangBaseComponent<string | numb
 
   _showOptions = signal<boolean | null>(null)
 
-  _inputValue = signal<string | null>(null)
+  _inputValue = signal<string>('')
 
   _optionHideTimeout = signal<any | undefined>(undefined)
 
@@ -131,12 +131,12 @@ export class QuangAutocompleteComponent extends QuangBaseComponent<string | numb
     return options.filter((x) => x.label.toLowerCase().includes(value.toLowerCase()))
   }
 
-  override onChangedHandler(value: string | number | null): void {
+  override onChangedHandler(value: string | number): void {
     super.onChangedHandler(value)
     this.setInputValue()
   }
 
-  onValueChange(value: string | number | null, hideOptions = true): void {
+  onValueChange(value: string | number, hideOptions = true): void {
     this.onChangedHandler(value)
     if (hideOptions) {
       this.hideOptionVisibility()
@@ -148,13 +148,13 @@ export class QuangAutocompleteComponent extends QuangBaseComponent<string | numb
     const option = this.selectOptions().find((x) => x.label.toLowerCase() === this._inputValue()?.toLowerCase())
     if (option?.value === this._value()) return
     if (option) {
-      this.onChangedHandler(option.value)
+      this.onChangedHandler(option.value ?? '')
     } else if (!this.syncFormWithText()) {
-      this.onChangedHandler(null)
+      this.onChangedHandler('')
     }
   }
 
-  override writeValue(val: string | number | null): void {
+  override writeValue(val: string | number): void {
     super.writeValue(val)
     this.setInputValue(true)
   }
@@ -173,7 +173,7 @@ export class QuangAutocompleteComponent extends QuangBaseComponent<string | numb
 
   setInputValue(resetOnMiss = false) {
     this._inputValue.set(
-      this.selectOptions().find((x) => x.value === this._value())?.label ?? (resetOnMiss ? null : this._inputValue())
+      this.selectOptions().find((x) => x.value === this._value())?.label ?? (resetOnMiss ? '' : this._inputValue())
     )
     if (!this.syncFormWithText()) this.inputValue$.next(this._inputValue() ?? '')
   }
