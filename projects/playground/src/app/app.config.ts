d3a083cbe218cc2c2755c012540f3a2754911b94
Matteo Cassano <matteo.cassano@quix.it>
2024-12-12 19:38:39 +0100

    feat(playground): update app.config with new providers

--- projects/playground/src/app/app.config.ts
+++ projects/playground/src/app/app.config.ts
@@ -6,7 +6,7 @@ import { provideRouter } from '@angular/router'
 import { provideAngularSvgIcon } from 'angular-svg-icon'
 
 import { provideQuangConfig } from '@quix/quang'
-import { withAuth } from '@quix/quang/auth'
+import { logoutOnErrorInterceptor, withAuth, withLogoutOnError, withSessionStorage } from '@quix/quang/auth'
 import { quangLoaderInterceptor, withLoaderExcludedUrls } from '@quix/quang/loader'
 import { withTranslation } from '@quix/quang/translation'
 
@@ -31,22 +31,26 @@ export const appConfig: ApplicationConfig = {
           method: 'GET',
         },
       ]),
-      withAuth({
-        issuer: 'https://demo.duendesoftware.com',
-        clientId: 'interactive.public', // The "Auth Code + PKCE" client
-        responseType: 'code',
-        redirectUri: `${window.location.origin}/`,
-        scope: 'openid profile email api offline_access', // Ask offline_access to support refresh token refreshes
-        useSilentRefresh: false, // Explicitly set this to false, otherwise code flow will try to use an iframe to refresh session
-        timeoutFactor: 0.25, // For faster testing
-        sessionChecksEnabled: true,
-        showDebugInformation: false, // Also requires enabling "Verbose" level in devtools
-        clearHashAfterLogin: false, // https://github.com/manfredsteyer/angular-oauth2-oidc/issues/457#issuecomment-431807040,
-        nonceStateSeparator: 'semicolon', // Real semicolon gets mangled by Duende ID Server's URI encoding,
-        sendAccessToken: true,
-        urlsToSendToken: ['https://demo.duendesoftware.com/api'],
-        autoLogin: false, // set this to true to automatically log in
-      })
+      withAuth(
+        {
+          issuer: 'https://demo.duendesoftware.com',
+          clientId: 'interactive.public', // The "Auth Code + PKCE" client
+          responseType: 'code',
+          redirectUri: `${window.location.origin}/`,
+          scope: 'openid profile email api offline_access', // Ask offline_access to support refresh token refreshes
+          useSilentRefresh: false, // Explicitly set this to false, otherwise code flow will try to use an iframe to refresh session
+          timeoutFactor: 0.25, // For faster testing
+          sessionChecksEnabled: true,
+          showDebugInformation: false, // Also requires enabling "Verbose" level in devtools
+          clearHashAfterLogin: false, // https://github.com/manfredsteyer/angular-oauth2-oidc/issues/457#issuecomment-431807040,
+          nonceStateSeparator: 'semicolon', // Real semicolon gets mangled by Duende ID Server's URI encoding,
+          sendAccessToken: true,
+          urlsToSendToken: ['https://demo.duendesoftware.com/api'],
+          autoLogin: false, // set this to true to automatically log in
+        },
+        withSessionStorage(),
+        withLogoutOnError([], [401, 402, 403])
+      )
     ),
   ],
 }
