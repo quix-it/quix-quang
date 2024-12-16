653fc72388873ed9e0e49f0a996adc1f4c6af484
manuel.pirrone <manuel.pirrone@quix.it>
2024-12-16 17:57:07 +0100

    feat: scroll to selected

--- projects/quang/components/shared/option-list/option-list.component.ts
+++ projects/quang/components/shared/option-list/option-list.component.ts
@@ -110,6 +110,8 @@ export class QuangOptionListComponent {
   optionList$ = effect(() => {
     if (this.optionList() && this.parentType() === OptionListParentType.SELECT) {
       this.optionList()?.nativeElement.focus()
+      const item = this.optionList()?.nativeElement?.children[0]?.children.item(this.selectedElementIndex())
+      this.optionList()?.nativeElement.scroll(0, item?.getBoundingClientRect()?.top ?? 0)
     }
     const ul = this.optionList()?.nativeElement?.children[0]
     const li = ul.children
