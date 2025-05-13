450bab8cf6c6b37d40d9eadc4ab13c4633b8f40f
manuel.pirrone <manuel.pirrone@quix.it>
2025-05-13 17:49:37 +0200

    feat: add download functions to data handling

--- projects/playground/src/app/app.routes.ts
+++ projects/playground/src/app/app.routes.ts
@@ -1,28 +1,25 @@
 import { Routes } from '@angular/router'
 
-import { DataHandlingComponent } from './pages/data-handling/data-handling.component'
-import { HomeComponent } from './pages/home/home.component'
-
 export const routes: Routes = [
   {
     path: 'components',
-    loadChildren: () => import('./pages/components-test-pages/components-test-pages.routes'),
+    loadChildren: () => import('./pages/components-test-pages/components-test-pages.routes').then((m) => m.default),
   },
   {
     path: 'overlay',
-    loadChildren: () => import('./pages/overlay-test-pages/overlay-test-pages.routes'),
+    loadChildren: () => import('./pages/overlay-test-pages/overlay-test-pages.routes').then((m) => m.default),
   },
   {
     path: 'auth',
-    loadChildren: () => import('./pages/auth-test-pages/auth-test-pages.routes'),
+    loadChildren: () => import('./pages/auth-test-pages/auth-test-pages.routes').then((m) => m.default),
   },
   {
     path: '',
-    component: HomeComponent,
+    loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent),
   },
   {
     path: 'data-handling',
-    component: DataHandlingComponent,
+    loadComponent: () => import('./pages/data-handling/data-handling.component').then((m) => m.DataHandlingComponent),
   },
   {
     path: '**',
