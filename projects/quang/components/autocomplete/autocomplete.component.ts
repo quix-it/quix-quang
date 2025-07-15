726ea9073536a6e3f0a2211ec190751df1e18c8d
Stefano Restuccia <stefano.restuccia@quix.it>
2025-07-15 10:33:55 +0200

    fix(autocomplete): clean up imports and remove debug logs in selectOptionsChange subscription

--- projects/quang/components/autocomplete/autocomplete.component.ts
+++ projects/quang/components/autocomplete/autocomplete.component.ts
@@ -97,11 +97,9 @@ export class QuangAutocompleteComponent extends QuangBaseComponent<string | numb
     .pipe(takeUntilDestroyed())
     .subscribe(() => {
       const value = this._value()
-      console.log('selectOptionsChange', this.selectOptions())
       if (!this.multiple() && !this._inputValue()) {
         this.setInputValue()
       } else if (this.multiple() && Array.isArray(value) && value.length > 0) {
-        console.log('value', value)
         for (const valueElement of value) {
           this.onSelectValue(valueElement)
         }
